c1324ec1ab8539b57e8495983df12588867aea01
Almino Melo <almino.melo@gmail.com>
2016-06-08 16:42:16 -0300

    Um cabeçalho agora pode ser um divisor

--- Header.php
+++ Header.php
@@ -1,13 +1,23 @@
 <?php
+
 namespace Onimla\SemanticUI;
 
-
 class Header extends \Onimla\HTML\Heading {
-    use Traits\Component, Traits\Alignment, Traits\Colored;
-    
+
+    const CLASS_VALUE = 'header';
+
+    use Traits\Component,
+        Traits\Alignment,
+        Traits\Colored;
+
     public function __construct($number, $text = FALSE, $class = FALSE) {
         parent::__construct($number, $text, $class);
         $this->setComponent();
-        $this->addClass('header');
+        $this->addClass(self::CLASS_VALUE);
     }
+
+    public function dividing() {
+        return $this->getClass()->before(self::CLASS_VALUE, __FUNCTION__);
+    }
+
 }
