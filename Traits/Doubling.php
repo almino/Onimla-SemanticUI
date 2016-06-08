92199c310967e5c4164b155630ca15d561d20ab3
Almino Melo <almino.melo@gmail.com>
2016-06-08 19:05:17 -0300

    Ops, eram traits e não classes.

--- Traits/Doubling.php
+++ Traits/Doubling.php
@@ -2,19 +2,19 @@
 
 namespace Onimla\SemanticUI\Traits;
 
-
 trait Doubling {
 
     public function doubling() {
         return $this->setDoubling();
     }
-    
+
     public function setDoubling() {
         $this->getClass()->prepend(\Onimla\SemanticUI\Constant::DOUBLING);
         return $this;
     }
-    
+
     public function unsetDoubling() {
         return $this->removeClass(\Onimla\SemanticUI\Constant::DOUBLING);
     }
+
 }
