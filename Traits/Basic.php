e9826000e9bafa70f0e35d72ca684b0d0c572fdb
Almino Melo <almino.melo@gmail.com>
2016-07-14 15:24:17 -0300

    Posiciona corretamente o identificador da classe.

--- Traits/Basic.php
+++ Traits/Basic.php
@@ -2,15 +2,23 @@
 
 namespace Onimla\SemanticUI\Traits;
 
+use Onimla\SemanticUI\Constant;
+use Onimla\SemanticUI\Label;
+
 trait Basic {
 
     private function basicAddClass($class) {
         $method = 'after';
         $search = \Onimla\SemanticUI\Component::CLASS_NAME;
 
-        if ($this->hasClass(\Onimla\SemanticUI\Constant::BUTTON)) {
+        if ($this->hasClass(Constant::BUTTON)) {
             $method = 'before';
-            $search = \Onimla\SemanticUI\Constant::BUTTON;
+            $search = Constant::BUTTON;
+        }
+
+        if ($this->hasClass(Label::CLASS_NAME)) {
+            $method = 'before';
+            $search = Label::CLASS_NAME;
         }
 
         call_user_func_array(array($this->getClassAttribute(), $method), array($search, func_get_args()));
@@ -19,16 +27,20 @@ trait Basic {
     }
 
     public function basic() {
-        return $this->setBasic();
-    }
-
-    public function setBasic() {
-        $this->basicAddClass(\Onimla\SemanticUI\Constant::BASIC);
+        $this->setBasic();
         return $this;
     }
 
+    public function setBasic() {
+        $this->basicAddClass(Constant::BASIC);
+    }
+
     public function unsetBasic() {
-        return $this->removeClass(\Onimla\SemanticUI\Constant::BASIC);
+        $this->removeClass(Constant::BASIC);
+    }
+    
+    public function isBasic() {
+        return $this->hasClass(Constant::BASIC);
     }
 
 }
