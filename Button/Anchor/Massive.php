17eac823ff1d0ebb474ed28d25578be54fda52cc
Almino Melo <almino.melo@gmail.com>
2016-07-18 17:00:17 -0300

    Ops

--- Button/Anchor/Massive.php
+++ Button/Anchor/Massive.php
@@ -7,5 +7,6 @@ use Onimla\SemanticUI\Button\Anchor as Button;
 class Massive extends Button {
     public function __construct($text = FALSE, $href = FALSE, $title = FALSE) {
         parent::__construct($text, $href, $title);
+        $this->massive();
     }
 }
