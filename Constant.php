2ca58c45d7178eddbbf9f523373d91ecdc8a842d
Almino Melo <almino.melo@gmail.com>
2016-07-18 14:26:46 -0300

    Um segmento pode ser elevado ou circular.

--- Constant.php
+++ Constant.php
@@ -8,13 +8,22 @@ abstract class Constant {
     const STACKABLE = 'stackable';
     const BUTTON = 'button';
     const BASIC = 'basic';
-    
+
     # http://semantic-ui.com/elements/label.html
     const BELOW = 'below';
     const LEFT = 'left';
     const RIGHT = 'right';
-    
+
     # http://semantic-ui.com/modules/dropdown.html#search-selection
     const SEARCH = 'search';
+
+    # http://semantic-ui.com/elements/segment.html#vertical-segment
+    const VERTICAL = 'vertical';
+
+    # http://semantic-ui.com/elements/segment.html#raised
+    const RAISED = 'raised';
+    
+    # http://semantic-ui.com/elements/segment.html#circular
+    const CIRCULAR = 'circular';
 
 }
--- Segment.php
+++ Segment.php
@@ -2,12 +2,15 @@
 
 namespace Onimla\SemanticUI;
 
+use Onimla\SemanticUI\Constant;
+
 /**
  * A segment is used to create a grouping of related content
  */
 class Segment extends Component {
 
-    use Traits\Attached,
+    use Traits\Alignment,
+        Traits\Attached,
         Traits\Colored;
 
     const CLASS_NAME = 'segment';
@@ -15,6 +18,56 @@ class Segment extends Component {
     public function __construct($children = FALSE) {
         parent::__construct('div', FALSE, func_get_args());
         $this->addClass(self::CLASS_NAME);
+    }
+    
+    public function setVertical() {
+        $this->getClassAttribute()->before(self::CLASS_NAME, Constant::VERTICAL);
+    }
+
+    public function unsetVertical() {
+        $this->removeClass(Constant::VERTICAL);
+    }
+
+    public function isVertical() {
+        return $this->hasClass(Constant::VERTICAL);
+    }
+
+    public function vertical() {
+        $this->setVertical();
+    }
+    
+    public function setRaised() {
+        $this->getClassAttribute()->before(self::CLASS_NAME, Constant::RAISED);
+    }
+
+    public function unsetRaised() {
+        $this->removeClass(Constant::RAISED);
+    }
+
+    public function isRaised() {
+        return $this->hasClass(Constant::RAISED);
+    }
+
+    public function raised() {
+        $this->setRaised();
+        return $this;
+    }
+    
+    public function setCircular() {
+        $this->getClassAttribute()->before(self::CLASS_NAME, Constant::CIRCULAR);
+    }
+
+    public function unsetCircular() {
+        $this->removeClass(Constant::CIRCULAR);
+    }
+
+    public function isCircular() {
+        return $this->hasClass(Constant::CIRCULAR);
+    }
+
+    public function circular() {
+        $this->setCircular();
+        return $this;
     }
 
 }
