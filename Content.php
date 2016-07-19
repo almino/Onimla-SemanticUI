0c8397176d86769b9c3b981d7a25f1b36589a018
Almino Melo <almino.melo@gmail.com>
2016-07-19 15:06:59 -0300

    Só um atalho para usar em outros componentes.

---
+++ Content.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Onimla\SemanticUI;
+
+use Onimla\HTML\Element;
+
+class Content extends Element {
+
+    const CLASS_NAME = 'content';
+
+    public function __construct($children = FALSE) {
+        parent::__construct('div', FALSE, $children);
+        $this->addClass(self::CLASS_NAME);
+    }
+
+}
