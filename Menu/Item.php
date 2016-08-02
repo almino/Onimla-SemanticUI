109dd618a54b8bfbeecbc839b66b642382018c12
Almino Melo <almino.melo@gmail.com>
2016-08-02 19:14:42 -0300

    Correção de erro.

--- Menu/Item.php
+++ Menu/Item.php
@@ -5,16 +5,17 @@ namespace Onimla\SemanticUI\Menu;
 use Onimla\HTML\Anchor;
 use Onimla\SemanticUI\Traits\Active;
 use Onimla\SemanticUI\Traits\Colored;
-use Onimla\SemanticUI\Traits\Item;
+use Onimla\SemanticUI\Traits\Item as tItem;
 
 class Item extends Anchor {
 
     use Active,
         Colored,
-        Item;
+        tItem;
 
-    public function __construct($text, $title = FALSE) {
-        parent::__construct($text, $title);
+    public function __construct($text = FALSE, $href = FALSE, $title = FALSE) {
+        parent::__construct($text, $href, $title);
+        $this->setItem();
     }
 
 }
