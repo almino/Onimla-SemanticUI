51ef3995afd591c64556f7eaac3b9bfe3a2894bc
Almino Melo <almino.melo@gmail.com>
2016-08-03 18:47:44 -0300

    Atalho para o ícone com balão de fala.

---
+++ Icon/Outline/Talk.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Onimla\SemanticUI\Icon\Outline;
+
+use Onimla\SemanticUI\Icon\Outline as Icon;
+use Onimla\SemanticUI\Icon\Talk as BasicTalk;
+
+class Talk extends Icon {
+
+    public function __construct() {
+        parent::__construct(BasicTalk::CLASS_NAME);
+    }
+
+}
