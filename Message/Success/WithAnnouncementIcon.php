4201c5bd5aec1f8ea30331c01b93be56c3573866
Almino Melo <almino.melo@gmail.com>
2016-08-19 18:30:25 -0300

    Correção do nome da classe.

--- Message/Success/WithAnnouncementIcon.php
+++ Message/Success/WithAnnouncementIcon.php
@@ -6,8 +6,10 @@ use Onimla\SemanticUI\Message\Success as Message;
 use Onimla\SemanticUI\Icon\Announcement as Icon;
 
 class WithAnnouncementIcon extends Message {
+
     public function __construct($text = FALSE) {
         parent::__construct($text);
         $this->icon(new Icon);
     }
+
 }
--- Message/Success/WithCheckmarkIcon.php
+++ Message/Success/WithCheckmarkIcon.php
@@ -5,9 +5,11 @@ namespace Onimla\SemanticUI\Message\Success;
 use Onimla\SemanticUI\Message\Success as Message;
 use Onimla\SemanticUI\Icon\Checkmark as Icon;
 
-class WithAnnouncementIcon extends Message {
+class WithCheckmarkIcon extends Message {
+
     public function __construct($text = FALSE) {
         parent::__construct($text);
         $this->icon(new Icon);
     }
+
 }
