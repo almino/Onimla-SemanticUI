bd8692752b67d37496d60351e8dd6c0ebc12942e
Almino Melo <almino.melo@gmail.com>
2016-08-23 16:44:10 -0300

    Atalhos para posições dos marcadores. Melhora a remoção das classes.

--- Traits/Labeled.php
+++ Traits/Labeled.php
@@ -13,19 +13,52 @@ trait Labeled {
         $this->getClassAttribute()->before(self::CLASS_NAME, $position, Constant::LABELED);
     }
 
-    public function unsetLabeled() {
-        $this->getClassAttribute()->strictRemoveClass(Constant::LEFT, Constant::LABELED);
-        $this->getClassAttribute()->strictRemoveClass(Constant::RIGHT, Constant::LABELED);
-        $this->removeClass(Constant::LABELED);
+    public function unsetLabeled($position = FALSE) {
+        $this->getClassAttribute()->strictRemoveClass($this->getLabeledClasses($position));
     }
 
-    public function isLabeled() {
-        return $this->hasClass(Constant::LABELED);
+    private function labeledRegEx($position = FALSE) {
+        if ($position === FALSE) {
+            $position = implode('|', array(
+                Constant::RIGHT,
+                Constant::LEFT,
+            ));
+        }
+
+        return "/({$position})\s+(" . Constant::LABELED . ')/';
+    }
+
+    public function getLabeledClasses($position = FALSE) {
+        $matches = array();
+
+        $this->getClassAttribute()->matchValue($this->labeledRegEx($position), $matches);
+
+        return count($matches) > 1 ? "{$matches[1]} {$matches[2]}" : NULL;
+    }
+
+    public function isLabeled($side = FALSE) {
+        return (bool) $this->getClassAttribute()->matchValue($this->labeledRegEx($side));
+    }
+
+    public function isLeftLabeled() {
+        return $this->isLabeled(Constant::LEFT);
+    }
+
+    public function isRightLabeled() {
+        return $this->isLabeled(Constant::RIGHT);
     }
 
     public function labeled($position = FALSE) {
         $this->setLabeled($position);
         return $this;
+    }
+
+    public function leftLabeled() {
+        return $this->labeled(Constant::LEFT);
+    }
+
+    public function rightLabeled() {
+        return $this->labeled(Constant::RIGHT);
     }
 
 }
