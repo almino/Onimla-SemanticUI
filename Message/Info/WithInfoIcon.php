4fece91850208ea40a069a34f42d3a2cea1d92c2
Almino Melo <almino.melo@gmail.com>
2016-09-05 19:50:15 -0300

    Ops

--- Message/Info/WithInfoIcon.php
+++ Message/Info/WithInfoIcon.php
@@ -2,7 +2,7 @@
 
 namespace Onimla\SemanticUI\Message\Info;
 
-use Onimla\SemanticUI\Message\Success as Message;
+use Onimla\SemanticUI\Message\Info as Message;
 use Onimla\SemanticUI\Icon\Circle\Info as Icon;
 
 class WithInfoIcon extends Message {
