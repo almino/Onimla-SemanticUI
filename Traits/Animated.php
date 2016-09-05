1e371166baea85b53a1f4d23399f9ef6684a1167
Almino Melo <almino.melo@gmail.com>
2016-09-05 17:20:32 -0300

    Correção de erro.

--- Traits/Animated.php
+++ Traits/Animated.php
@@ -7,7 +7,17 @@ use Onimla\SemanticUI\Constant;
 trait Animated {
 
     public function setAnimated() {
-        $this->getClassAttribute()->before(self::CLASS_NAME, Constant::ANIMATED);
+        $method = 'after';
+        $search = \Onimla\SemanticUI\Component::CLASS_NAME;
+
+        if ($this->hasClass(Constant::ITEM)) {
+            $method = 'before';
+            $search = Constant::ITEM;
+        }
+
+        call_user_func_array(array($this->getClassAttribute(), $method), array($search, func_get_args()));
+
+        return $this;
     }
     
     public function animate() {
