2f043000364128b84005e81b5fe5b04be53faada
Almino Melo <almino.melo@gmail.com>
2016-09-09 16:35:38 -0300

    Atalho para ícone com uma lupa.

---
+++ Icon/Search.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Onimla\SemanticUI\Icon;
+
+use Onimla\SemanticUI\Icon;
+
+class Search extends Icon {
+
+    const CLASS_NAME = 'search';
+
+    public function __construct() {
+        parent::__construct(self::CLASS_NAME);
+    }
+
+}
