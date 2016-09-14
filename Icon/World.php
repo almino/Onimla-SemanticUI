94ece279c94cb05a4ce979b05de41452d3f2ac03
Almino Melo <almino.melo@gmail.com>
2016-09-14 19:27:53 -0300

    Atalho para ícone com o Planeta Terra.

---
+++ Icon/World.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Onimla\SemanticUI\Icon;
+
+use Onimla\SemanticUI\Icon;
+
+class World extends Icon {
+
+    const CLASS_NAME = 'world';
+
+    public function __construct() {
+        parent::__construct(self::CLASS_NAME);
+    }
+
+}
