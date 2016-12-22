e7eab8ad174237449d263f352fe7f8c593d42119
Almino Melo <almino.melo@gmail.com>
2016-12-22 17:21:24 -0300

    Correção de erro.

--- Traits/Icon.php
+++ Traits/Icon.php
@@ -23,17 +23,17 @@ trait Icon
             $search = Constant::BUTTON;
         }
 
-        call_user_func_array(array($this->getClassAttribute(), $method), array($search, Constant::LABELED, Constant::ICON));
+        call_user_func_array(array($this->getClassAttribute(), $method), array($search, Constant::ICON));
     }
 
     public function unsetIcon()
     {
-        $this->getClassAttribute()->strictRemoveClass(Constant::LABELED, Constant::ICON);
+        $this->getClassAttribute()->removeClass(Constant::ICON);
     }
 
     public function isIcon()
     {
-        return $this->hasClass(Constant::LABELED, Constant::ICON);
+        return $this->hasClass(Constant::ICON);
     }
 
     public function icon()
