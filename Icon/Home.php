02902939a3b337660e47ef0254b3a474336ea2d1
Almino Melo <almino.melo@gmail.com>
2017-01-13 13:24:31 -0300

    Atalho para o ícone da casa.

---
+++ Icon/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Onimla\SemanticUI\Icon;
+
+use Onimla\SemanticUI\Icon as BaseIcon;
+
+class Home extends BaseIcon {
+
+    const CLASS_NAME = 'home';
+
+    public function __construct() {
+        parent::__construct(self::CLASS_NAME);
+    }
+
+}
